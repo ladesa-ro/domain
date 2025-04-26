48995d78b3bc8de5aa58eb09946ebce29c373d4d
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2025-04-26 11:33:12 -0400

    chore: init integrations/typescript

---
+++ .tools/quicktype.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail;
+
+# CONTAINER_FILE=$(cat <<'EOF'
+# FROM docker.io/oven/bun:1
+# RUN bun install -g quicktype@latest
+# ENTRYPOINT [ "quicktype" ]
+# EOF
+# )
+
+# echo "${CONTAINER_FILE}" | podman build -;
+
+# IMG_SHA=$(echo "${CONTAINER_FILE}" | podman build - -q;)
+
+# podman run --rm -v $(pwd):/pwd -w /pwd -it ${IMG_SHA} $@;
+
+bunx quicktype@latest $@;
